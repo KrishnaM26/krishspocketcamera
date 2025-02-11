f3aaba0d06dcc6519c04c097137fed91be5a35ba
KrishnaM26 <krishna.mehta26@yahoo.com>
2025-02-11 14:42:23 -0500

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
-
+import "../css/Footer.css"
 
 function Footer() {
     return(
-        <div>
-            <a href="https://github.com/KrishnaM26" target="_blank"><p>Krishna Mehta</p></a>
+        <div className="gh-tag">
+            <a  href="https://github.com/KrishnaM26" target="_blank">
+                <p className="footer-text">© Krishna Mehta</p>
+            </a>
         </div>
     )
 }
